7f035371603a2016f3c02cd8836c1f836c58ec56
Andrew Stasyuk <320x200@gmail.com>
2022-09-28 18:56:18 -0700

    Missed one greedy problem

--- epi_judge_python/making_change.py
+++ epi_judge_python/making_change.py
@@ -1,9 +1,18 @@
 from test_framework import generic_test
 
 
+COINS = [100, 50, 25, 10, 5, 1]
+
+
 def change_making(cents: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    if cents == 0: return 0
+    num_coins = 0
+    for c in COINS:
+        while cents >= c:
+            cents -= c
+            num_coins += 1
+            if cents == 0: return num_coins
+    raise ValueError()
 
 
 if __name__ == '__main__':
