c7b06ba15b0c6c4983502172cc62bfd5588a117d
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-04-10 03:35:54 -0500

    Div, mul, power

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    # result * x ^ y = constant
+    negative_power = y < 0
+    y = abs(y)
+    result = 1.0
+    while y > 0:
+        if y & 1 != 0:
+            result *= x
+            y -= 1
+        x *= x
+        y >>= 1
+    return 1 / result if negative_power else result
 
 
 if __name__ == '__main__':
--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def divide(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    assert x >= 0 and y > 0
+    result = 0
+    bit = 1
+    while x >= y << 1:
+        y <<= 1
+        bit <<= 1
+    while bit > 0:
+        if x >= y:
+            x -= y
+            result |= bit
+        y >>= 1
+        bit >>= 1
+    return result
 
 
 if __name__ == '__main__':
