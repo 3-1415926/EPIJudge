88cb82cd50ecca8aed2d75712131637bcca47d48
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-04-12 12:50:30 -0500

    Sudoku

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -5,7 +5,21 @@ from test_framework import generic_test
 
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
-    # TODO - you fill in here.
+    N = 3
+    NN = N * N
+    rows, cols, sqrs = [0] * NN, [0] * NN, [0] * NN
+    for r in range(len(partial_assignment)):
+        for c in range(len(partial_assignment[r])):
+            digit = partial_assignment[r][c]
+            if digit == 0: continue
+            mask = 1 << digit
+            if rows[r] & mask: return False
+            rows[r] |= mask
+            if cols[c] & mask: return False
+            cols[c] |= mask
+            sqr_idx = r // N * N + c // N
+            if sqrs[sqr_idx] & mask: return False
+            sqrs[sqr_idx] |= mask
     return True
 
 
