3c7806fabbcda899c86d545b9d5a2238b865eb43
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-04-12 12:54:18 -0500

    Pascal triangle

--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -4,8 +4,15 @@ from test_framework import generic_test
 
 
 def generate_pascal_triangle(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    if n <= 0:
+        return []
+    rows = [[1]]
+    for r in range(2, n + 1):
+        row = []
+        for c in range(r):
+            row.append((rows[-1][c - 1] if c > 0 else 0) + (rows[-1][c] if c < r - 1 else 0))
+        rows.append(row)
+    return rows
 
 
 if __name__ == '__main__':
