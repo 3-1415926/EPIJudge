103468f14a1e445b415c7a221a87172e42f7f31b
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-04-16 20:51:15 -0700

    Lists bootcamp

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -6,8 +6,22 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    dummy_head = ListNode()
+    dummy_tail = dummy_head
+    while L1 and L2:
+        if L1.data <= L2.data:
+            dummy_tail.next = L1
+            dummy_tail = L1
+            L1 = L1.next
+        else:
+            dummy_tail.next = L2
+            dummy_tail = L2
+            L2 = L2.next
+    if L1:
+        dummy_tail.next = L1
+    elif L2:
+        dummy_tail.next = L2
+    return dummy_head.next
 
 
 if __name__ == '__main__':
