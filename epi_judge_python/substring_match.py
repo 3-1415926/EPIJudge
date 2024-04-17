71ab7a61ad1e7d0d0b8af0257710081007b4770f
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-04-16 20:35:37 -0700

    Rabin-Karp

--- epi_judge_python/substring_match.py
+++ epi_judge_python/substring_match.py
@@ -1,9 +1,25 @@
 from test_framework import generic_test
 
 
+BASE = 101
+MODULO = 1 << 32
+
+
 def rabin_karp(t: str, s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    if len(t) < len(s):
+        return -1
+    t_hash, s_hash, base_factor = 0, 0, 1
+    for i in range(len(s)):
+        t_hash = (t_hash * BASE + ord(t[i])) % MODULO
+        s_hash = (s_hash * BASE + ord(s[i])) % MODULO
+        base_factor = base_factor * BASE % MODULO
+    for i in range(len(s), len(t) + 1):
+        if t_hash == s_hash and t[i - len(s):i] == s:
+            return i - len(s)
+        if i >= len(t):
+            break
+        t_hash = (t_hash * BASE - ord(t[i - len(s)]) * base_factor + ord(t[i])) % MODULO
+    return -1
 
 
 if __name__ == '__main__':
