3e802cd7983ec554925f78a71e55dcca823b8e40
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-05-07 01:51:27 -0700

    Finished lists

--- epi_judge_python/int_as_list_add.py
+++ epi_judge_python/int_as_list_add.py
@@ -5,8 +5,21 @@ from test_framework import generic_test
 
 
 def add_two_numbers(L1: ListNode, L2: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if L1 is None: return L2
+    if L2 is None: return L1
+    dummy_head = tail = ListNode()
+    carry = 0
+    while L1 is not None or L2 is not None or carry != 0:
+        if L1 is not None:
+            carry += L1.data
+            L1 = L1.next
+        if L2 is not None:
+            carry += L2.data
+            L2 = L2.next
+        tail.next = ListNode(data=carry % 10)
+        tail = tail.next
+        carry //= 10
+    return ListNode(data=0) if dummy_head.next is None else dummy_head.next
 
 
 if __name__ == '__main__':
