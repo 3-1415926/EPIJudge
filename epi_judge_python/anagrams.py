528c99a6cbd3af7ad853c920bddc0113dd7a6c59
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-05-09 14:17:03 -0700

    Starting hashtables

--- epi_judge_python/anagrams.py
+++ epi_judge_python/anagrams.py
@@ -1,11 +1,14 @@
+import collections
 from typing import List
 
 from test_framework import generic_test, test_utils
 
 
 def find_anagrams(dictionary: List[str]) -> List[List[str]]:
-    # TODO - you fill in here.
-    return []
+    groups = collections.defaultdict(list)
+    for item in dictionary:
+        groups[''.join(sorted(item))].append(item)
+    return [g for g in groups.values() if len(g) > 1]
 
 
 if __name__ == '__main__':
--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,9 +1,12 @@
+import collections
 from test_framework import generic_test
 
 
 def can_form_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    counter = collections.Counter()
+    for c in s:
+        counter[c] += 1
+    return sum(v % 2 for v in counter.values()) <= 1
 
 
 if __name__ == '__main__':
