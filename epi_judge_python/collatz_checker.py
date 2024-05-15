1dd1ce86b284cdbf41733253655268882a300080
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-05-15 12:16:42 -0700

    Collatz

--- epi_judge_python/collatz_checker.py
+++ epi_judge_python/collatz_checker.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def test_collatz_conjecture(n: int) -> bool:
-    # TODO - you fill in here.
-    return False
+    assert n >= 1
+    already_true = set()
+    for i in range(3, n + 1, 2):
+        current_sequence = set()
+        j = i
+        while j >= i and j not in already_true:
+            current_sequence.add(j)
+            if j % 2 == 0:
+                j //= 2
+            else:
+                j = 3 * j + 1
+        already_true.update(current_sequence)
+        already_true.discard(i)
+    return True
 
 
 if __name__ == '__main__':
