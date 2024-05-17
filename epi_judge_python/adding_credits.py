ee1e91ff5905c0e35b8fbb4d7a084a30bcde804c
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-05-17 13:55:54 -0700

    Credits

--- epi_judge_python/adding_credits.py
+++ epi_judge_python/adding_credits.py
@@ -1,27 +1,37 @@
+import heapq
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
 class ClientsCreditsInfo:
+    def __init__(self):
+        self.all_delta = 0
+        self.data = {}
+        self.heap = []
+    
     def insert(self, client_id: str, c: int) -> None:
-        # TODO - you fill in here.
-        return
+        self.data[client_id] = c - self.all_delta
+        heapq.heappush(self.heap, (self.all_delta - c, client_id))
 
     def remove(self, client_id: str) -> bool:
-        # TODO - you fill in here.
-        return True
+        old_c = self.data.pop(client_id, None)
+        return old_c is not None
 
     def lookup(self, client_id: str) -> int:
-        # TODO - you fill in here.
-        return 0
+        c = self.data.get(client_id)
+        return -1 if c is None else c + self.all_delta
 
     def add_all(self, C: int) -> None:
-        # TODO - you fill in here.
-        return
+        self.all_delta += C
 
     def max(self) -> str:
-        # TODO - you fill in here.
-        return ''
+        while self.heap:
+            c, client_id = heapq.heappop(self.heap)
+            old_c = self.data.get(client_id)
+            if old_c is not None and old_c == -c:
+                return client_id            
+        if not self.heap:
+            raise ValueError()
 
 
 def client_credits_info_tester(ops):
