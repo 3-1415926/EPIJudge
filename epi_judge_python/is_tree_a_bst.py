4ba76cbb95563cb3c603203236698701e6f00e1a
3-1415926 <962690+3-1415926@users.noreply.github.com>
2024-05-17 01:23:34 -0700

    Is a bst

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,10 +1,20 @@
+from typing import Tuple
+
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+def is_bst_with_range(tree: BinaryTreeNode) -> Tuple[bool, int, int]:
+    if not tree:
+        return True, None, None
+    left_bst, left_min, left_max = is_bst_with_range(tree.left)
+    right_bst, right_min, right_max = is_bst_with_range(tree.right)
+    return (left_bst and right_bst and (left_max <= tree.data if tree.left else True) and (tree.data <= right_min if tree.right else True),
+            left_min if tree.left else tree.data,
+            right_max if tree.right else tree.data) 
+
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    return is_bst_with_range(tree)[0]
 
 
 if __name__ == '__main__':
