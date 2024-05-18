154809c8da92188030cca56235ecdb566f2da258
3-1415926 <962690+3-1415926@users.noreply.github.com>
2024-05-17 23:47:51 -0700

    String in matrix

--- epi_judge_python/is_string_in_matrix.py
+++ epi_judge_python/is_string_in_matrix.py
@@ -2,11 +2,29 @@ from typing import List
 
 from test_framework import generic_test
 
+DR, DC = [0, -1, 0, 1], [1, 0, -1, 0]
+
 
 def is_pattern_contained_in_grid(grid: List[List[int]],
                                  pattern: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+    if not pattern:
+        return False
+    prev, cur = [[False] * len(r) for r in grid], [[False] * len(r) for r in grid]
+    for i in range(len(pattern)):
+        prev, cur = cur, prev
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                cur[r][c] = False
+                if grid[r][c] != pattern[i]:
+                    continue
+                if i == 0:
+                    cur[r][c] = True
+                    continue
+                for dr, dc in zip(DR, DC):
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < len(grid) and 0 <= nc < len(grid[r]):
+                        cur[r][c] |= prev[nr][nc]
+    return any(any(r) for r in cur)
 
 
 if __name__ == '__main__':
