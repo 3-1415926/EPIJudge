f218b5dd8b632d99792c0c8475d1b3f32fc492b1
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-05-17 17:47:15 -0700

    Combinations

--- epi_judge_python/combinations.py
+++ epi_judge_python/combinations.py
@@ -4,8 +4,23 @@ from test_framework import generic_test, test_utils
 
 
 def combinations(n: int, k: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    combination = [i < k for i in range(n)]
+    make_combination = lambda: [(i + 1) for i, x in enumerate(combination) if x]
+    results = [make_combination()]
+    while True:
+        i = n - 1
+        while i > 0 and (not combination[i - 1] or combination[i]):
+            i -= 1
+        if i <= 0:
+            break
+        combination[i - 1], combination[i] = combination[i], combination[i - 1]
+        i, j = i + 1, n - 1
+        while i < j:
+            combination[i], combination[j] = combination[j], combination[i]
+            i += 1
+            j -= 1
+        results.append(make_combination())
+    return results
 
 
 if __name__ == '__main__':
