9ec0cafadc7a1611fee458a16f856d724527daa2
3-1415926 <962690+3-1415926@users.noreply.github.com>
2024-05-19 14:53:04 -0700

    Zip list

--- epi_judge_python/zip_list.py
+++ epi_judge_python/zip_list.py
@@ -5,8 +5,25 @@ from test_framework import generic_test
 
 
 def zipping_linked_list(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    slow_head = None
+    slow = fast = L
+    while fast:
+        fast = fast.next
+        slow.next, slow_head, slow = slow_head, slow, slow.next
+        if fast:
+            fast = fast.next
+        else:
+            break
+    else:
+        slow_head, slow = slow, slow_head
+    result = None
+    while slow_head:
+        slow_head.next, result, slow_head = result, slow_head, slow_head.next
+        if slow:
+            slow.next, result, slow = result, slow, slow.next
+        else:
+            break
+    return result
 
 
 if __name__ == '__main__':
