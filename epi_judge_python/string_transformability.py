458eeb08412e350933b5094e0b5b1d4a8ccde74f
3-1415926 <962690+3-1415926@users.noreply.github.com>
2024-05-19 02:07:11 -0700

    Penultimate graph

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,11 +1,24 @@
+from collections import deque
 from typing import Set
 
 from test_framework import generic_test
 
 
 def transform_string(D: Set[str], s: str, t: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    queue = deque([(s, 0)])
+    D.remove(s)
+    while queue:
+        word, dist = queue.popleft()
+        if word == t:
+            return dist
+        for k in range(len(word)):
+            for c in range(ord('a'), ord('z') + 1):
+                d = word[:k] + chr(c) + word[k+1:]
+                if d in D:
+                    D.remove(d)
+                    queue.append((d, dist + 1))
+    return -1
+
 
 
 if __name__ == '__main__':
