625191fc91647dd404d4c68e9336ac7211f34a4e
3-1415926 <962690+3-1415926@users.noreply.github.com>
2024-05-19 08:46:33 -0700

    Finished graphs

--- epi_judge_python/max_teams_in_photograph.py
+++ epi_judge_python/max_teams_in_photograph.py
@@ -1,20 +1,50 @@
+import collections
 import functools
-from typing import List
+from typing import List, Optional
 
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
+UNVISITED, PROCESSING, VISITED = range(3)
+
+
 class GraphVertex:
     def __init__(self) -> None:
         self.edges: List[GraphVertex] = []
-        # Set max_distance = 0 to indicate unvisitied vertex.
-        self.max_distance = 0
+        # Set max_distance = UNVISITED to indicate unvisitied vertex.
+        self.max_distance = UNVISITED
+
+
+def top_sort(graph: List[GraphVertex]) -> Optional[List[GraphVertex]]:
+    top_sorted = []
+    def dfs(vertex: GraphVertex) -> bool:
+        if vertex.max_distance == VISITED:
+            return True
+        if vertex.max_distance == PROCESSING:
+            return False
+        vertex.max_distance = PROCESSING
+        for edge in vertex.edges:
+            dfs(edge)
+        vertex.max_distance = VISITED
+        top_sorted.append(vertex)
+        return True
+    for vertex in graph:
+        if not dfs(vertex):
+            return None
+    top_sorted.reverse()
+    return top_sorted
 
 
 def find_largest_number_teams(graph: List[GraphVertex]) -> int:
-    # TODO - you fill in here.
-    return 0
+    graph = top_sort(graph)
+    if graph is None:
+        return 0
+    longest_path = collections.defaultdict(int)
+    for vertex in graph:
+        for edge in vertex.edges:
+            longest_path[edge] = max(longest_path[edge], longest_path[vertex] + 1)
+    return max(longest_path.values()) + 1
 
 
 @enable_executor_hook
